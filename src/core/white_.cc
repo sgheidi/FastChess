2b11b85d792f4207cbfbcceac361eb0910775340
Shervan Gheidi <shergheidi@gmail.com>
2020-11-26 09:44:52 -0700

    Static functions for Black/White namespaces

--- src/core/white_.cc
+++ src/core/white_.cc
@@ -4,7 +4,7 @@ namespace White {
 std::vector<std::vector<int>> blocks(8);
 std::vector<bool> en_passant(8);
 bool turn = true;
-static bool enpassant_check_killed = false;
+bool enpassant_check_killed = false;
 int num_queens = 1;
 std::vector<std::string> checker = {};
 bool screenshot = true;
