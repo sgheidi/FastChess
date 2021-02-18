cb32f2c11f707cbfb18b53c203483efb401e434a
Shervan Gheidi <shergheidi@gmail.com>
2021-02-18 09:07:42 -0800

    Virtual functions for piece interface

--- src/core/piece.h
+++ src/core/piece.h
@@ -1,15 +1,17 @@
 #ifndef CORE_PIECE_H
 #define CORE_PIECE_H
 
-// The abstract base class from which all other piece classes (King, Queen, Bishop,...etc) inherit attributes from
+// The interface for all piece classes.
 class Piece {
-protected:
 public:
   sf::Sprite sprite;
   sf::Texture texture;
   std::vector<int> row, col, y, x;
   std::vector<std::vector<std::vector<int>>> movelist, protecting_movelist, hit_movelist;
   std::vector<bool> alive;
+protected:
+  virtual void update_movelist() = 0;
+  virtual void show() = 0;
 };
 
 #endif // CORE_PIECE_H
